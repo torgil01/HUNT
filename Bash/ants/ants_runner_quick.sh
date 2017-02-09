8ad2c9fd40725c597d6596e32eae5f334ee7ee5d
torgil01 <torgil.vangberg@gmail.com>
2017-02-09 10:36:00 +0100

    First commit; moved from ~/bin

---
+++ Bash/ants/ants_runner_quick.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# condor wrapper for antsRegistrationSyN.sh
+# INPUT args <template> <mask> <output_stem>
+# NOTE: Use only filename, not extension for output image
+export ANTSPATH=/usr/share/ANTS
+TEMPLATE=$1
+MASK=$2
+img=$3
+outName=$4
+startDir=`pwd`
+dn=`dirname $img`
+cd $dn
+$ANTSPATH/antsRegistrationSyNQuick.sh -d 3 -t s -f $TEMPLATE -x $MASK -m $img -n 1 -p d -o $outName
+cd $startDir
