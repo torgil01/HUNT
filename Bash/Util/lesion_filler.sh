b206353fdece8e99fb903f4e35cdbeedd3205c00
torgil01 <torgil.vangberg@gmail.com>
2017-02-09 10:33:27 +0100

    lesion mask is now made from aseg-hypointensities and/or wmh depending on which are available

--- Bash/Util/lesion_filler.sh
+++ Bash/Util/lesion_filler.sh
@@ -13,7 +13,7 @@ source /etc/fsl/5.0/fsl.sh
 
 
 # parse args
-while getopts "hd:w:t:f:m:" flag
+while getopts "hd:w:t:f:m:a:" flag
 do
   case "$flag" in
     d)
@@ -31,6 +31,9 @@ do
     m)
       wmMask=$OPTARG
       ;;
+    a)
+	aseg=$OPTARG
+	;;
     h|?)
       echo $flag
       usage
@@ -39,38 +42,86 @@ do
   esac
 done
 
-
 cd ${t1AntsDir}
-maskSmoothedWMH=${t1AntsDir}/wmh_sigma1.nii.gz
-tmpImg=${t1AntsDir}/wmh_sigma1-tmp.nii.gz
-
-aseg=${t1AntsDir}/fsAseg.nii.gz # aseg.mgz from freesurfer 
-if [ -e ${aseg} ]; then
-    tmp2Img=${t1AntsDir}/aseg_tmp.nii.gz
+lesionMask=${t1AntsDir}/lesionMask.nii.gz
+tmpImg=${t1AntsDir}/tmp1.nii.gz
+lesionMaskInfo=${t1AntsDir}/lesionMask.info
+# 4 possible cases
+# case 1 wmh and aseg
+if [ -e ${aseg} ] && [ -e ${wmh} ] ; then
+    tmp2Img=${t1AntsDir}/tmp2.nii.gz
+    tmp3Img=${t1AntsDir}/tmp3.nii.gz
+    tmp4Img=${t1AntsDir}/tmp4.nii.gz
+    hypo=${t1AntsDir}/fsHypointensities.nii.gz
+    lWM=${t1AntsDir}/lWM.nii.gz
+    rWM=${t1AntsDir}/rWM.nii.gz
+    wm=${t1AntsDir}/wmMask.nii.gz
+    # cut the wmh with a wm mask so that no part of the lesion mask is outside wm
+    # SPM wm segmentation is unreliable here, so we assemble a wm mask from FS aseg file
+    # wmMask = leftWM + rightWM + wmHypo = labels (2 + 41 + 77)
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 41 -thr 41 -bin ${rWM}
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 2 -thr 2 -bin ${lWM}
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 77 -thr 77 -bin ${hypo}
+    ${FSL_BIN}/fslmaths ${hypo} -add ${lWM} -add ${rWM}  ${wm}            
+    # smooth wmh (tmp2Imag is output there  
+    ${ANTSPATH}/ImageMath 3 ${tmp2Img} G ${wmh} 1.5
+    # add hypo mask    
+    ${FSL_BIN}/fslmaths ${tmp2Img} -add ${hypo} ${tmpImg}
+    # add threshold   
+    ${FSL_BIN}/fslmaths  ${tmpImg} -thr 0.15 -bin  ${tmp3Img}
+    # cut non wm parts of lesion mask 
+    ${FSL_BIN}/fslmaths  ${tmp3Img} -mul ${wm} ${tmp4Img}
+    # but this is not perfect, so we add the non-smoothed wmh mask
+    ${FSL_BIN}/fslmaths  ${tmp4Img} -add ${wmh} -thr 0.9 -bin  ${lesionMask}
+    rm ${tmp2Img} ${tmpImg} ${tmp3Img} ${tmp4Img} ${rWM} ${lWM} ${wm}
+    echo "lesion mask created with aseg + wmh" >  $lesionMaskInfo
+fi
+# case 2 wmh only
+if [ ! -e ${aseg} ] && [ -e ${wmh} ] ; then
+    tmp2Img=${t1AntsDir}/tmp2.nii.gz
+    tmp3Img=${t1AntsDir}/tmp3.nii.gz
+    hypo=${t1AntsDir}/fsHypointensities.nii.gz
+    lWM=${t1AntsDir}/lWM.nii.gz
+    rWM=${t1AntsDir}/rWM.nii.gz
+    wm=${t1AntsDir}/wmMask.nii.gz
+    # cut the wmh with a wm mask so that no part of the lesion mask is outside wm
+    # SPM wm segmentation is unreliable here, so we assemble a wm mask from FS aseg file
+    # wmMask = leftWM + rightWM + wmHypo = labels (2 + 41 + 77)
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 41 -thr 41 -bin ${rWM}
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 2 -thr 2 -bin ${lWM}
+    ${FSL_BIN}/fslmaths ${aseg} -uthr 77 -thr 77 -bin ${hypo}
+    ${FSL_BIN}/fslmaths ${hypo} -add ${lWM} -add ${rWM}  ${wm}            
+    # smooth wmh mask
+    ${ANTSPATH}/ImageMath 3 ${tmpImg} G ${wmh} 1.5
+    ${FSL_BIN}/fslmaths  ${tmpImg} -thr 0.1.5 -bin  ${tmp2Img}
+    # cut non wm parts of lesion mask 
+    ${FSL_BIN}/fslmaths  ${tmp2Img} -mul ${wm} ${tmp3Img}
+    # but this is not perfect, so we add the non-smoothed wmh mask
+    ${FSL_BIN}/fslmaths  ${tmp3Img} -add ${wmh} -thr 0.9 -bin  ${lesionMask}
+    rm ${tmp2Img} ${tmpImg} ${tmp3Img} ${rWM} ${lWM}  ${wm}
+    echo "lesion mask created with wmh" >  $lesionMaskInfo
+fi
+# case 3 aseg only
+if [ -e ${aseg} ] && [ ! -e ${wmh} ] ; then
     hypo=${t1AntsDir}/fsHypointensities.nii.gz	
     ${FSL_BIN}/fslmaths ${aseg} -uthr 77 -thr 77 -bin ${hypo}
-    ${FSL_BIN}/fslmaths ${wmh} -add ${hypo} ${tmp2Img}
-    ${ANTSPATH}/ImageMath 3 ${tmpImg} G ${tmp2Img} 1
-    rm ${tmp2Img}
-else
-    ${ANTSPATH}/ImageMath 3 ${tmpImg} G ${wmh} 1
+    cp $hypo  $lesionMask
+    echo "lesion mask created with aseg" >  $lesionMaskInfo
 fi
-
-# Binarize w. fslmaths
-${FSL_BIN}/fslmaths  ${tmpImg} -thr 0.2 -bin  ${maskSmoothedWMH}
-
-# cleanup
-rm ${tmpImg}
+# case 4 none (we should not get here)
+if [ ! -e ${aseg} ] && [ ! -e ${wmh} ] ; then
+    exit 0
+fi
 
     
 # make T1 image w. lesions filled
 t1Filled=${t1AntsDir}/brain_lesionFilled.nii.gz
     
 
-# add wmh to wm seg uing FSL lesion_filling 
+# add wmh to wm seg using FSL lesion_filling 
 ${FSL_BIN}/lesion_filling -i ${t1File}\
 	       -o ${t1Filled}\
-	       -l ${maskSmoothedWMH}\
+	       -l ${lesionMask}\
 	       -w ${wmMask}
 # cleanup
 rm ${t1AntsDir}/brain_lesionFilled_inneronly.nii.gz
--- Bash/Util/submit_lesionFilling.sh
+++ Bash/Util/submit_lesionFilling.sh
@@ -1,52 +1,80 @@
 #!/bin/bash
 # condor script for lesion filler
+# We fill in T1 hypinitensities to avoid errors in
+# calulating the template warps with ants.
+# There are four possible cases
+# 1. wmh + aseg : use both to reate mask for lesion filler
+# 2. wmh        : use only wmh for creating mask
+# 3. aseg       : use only aseg for creatig mask
+# 4. none       : ignore
 
 
-imDir=/home/torgil/Projects/HUNT/Testing/wmh_mask/data/
+imDir=/home/torgil/Projects/HUNT/WorkData
 startDir=`pwd`
 t1Name=brain_t1w.nii.gz
 wmhMask=wmh_t1.nii.gz # this is the wml interpolated to subjects T1 image
 
 # first make a list of all studies with a wmh mask 
 # note the braces () which makes output a array
-wmhFiles=(`find $imDir -type f -name $wmhMask`)
+#wmhFiles=(`find $imDir -type f -name $wmhMask`)
+
+# loop over all dirs in imDir, note the /*/ which ensures
+# that we only list dirs 
+studyDirs=(`ls -d $imDir/*/`) 
 
 startDir=`pwd`
-
 jobScript=/home/torgil/Projects/HUNT/ProcessingScripts/Bash/Util/lesion_filler.sh
+# set up directory for condor jobs
 CONDORDIR=condor_`date +%Y%m%d_%H%M%S`
 mkdir $CONDORDIR
 
 i=1
-for wmh in ${wmhFiles[@]}; do
+for thisStudy in ${studyDirs[@]}; do
     # condor stuff
     condorJobFile=$CONDORDIR/job_$i.condor
     logfile=$CONDORDIR/job_$i.log
     outfile=$CONDORDIR/job_$i.output
     errfile=$CONDORDIR/job_$i.err
-    
+
     # set vars
-    flairSpmDir=`dirname $wmh`
-    flairDir=`dirname $flairSpmDir`
+    flairDir=${thisStudy}/FLAIR
+    flairSpmDir=${flairDir}/spm
+    if [ -d "$flairDir" ] &&  [ -d "$flairSpmDir"  ]; then
+	wmhFile=${flairSpmDir}/${wmhMask}
+    else
+	wmhFile=false
+    fi
+
+    
     # find the warps under T1_1 or T1_2
-    studyDirName=`dirname $flairDir`
-    ID=`basename ${studyDirName}`
     t1AntsDir=''
-    if [ -d $studyDirName/T1_1/ants ]; then
-	t1Dir=$studyDirName/T1_1
-    elif [ -d $studyDirName/T1_2/ants ]; then
-	    t1Dir=$studyDirName/T1_2	    
+    if [ -d ${thisStudy}/T1_1/ants ]; then
+	t1Dir=${thisStudy}/T1_1
+    elif [ -d ${thisStudy}/T1_2/ants ]; then
+	    t1Dir=${thisStudy}/T1_2	    
     else
        continue
     fi
     t1AntsDir=${t1Dir}/ants
     t1File=${t1Dir}/${t1Name}
-    wmMask=${t1Dir}/c2t1w.nii.gz 
+    wmMask=${t1Dir}/c2t1w.nii.gz
+    # if the wm mask is missing we skip the subject (unlikely)
+    if [ ! -e "$wmMask" ];  then
+       continue
+    fi
+
+    # check if we have a aseg file
+    asegFile=${t1AntsDir}/fsAseg.nii.gz
+    if [ ! -e "$asegFile" ];  then
+	asegFile=false
+    fi
+
+    
     t1Filled=${t1AntsDir}/brain_lesionFilled.nii.gz
 
     # configure and submit condor job
     # arguments 
-    jobArgs="-d $t1AntsDir -w $wmh -t $t1File -f $t1Filled -m $wmMask"
+    jobArgs="-d $t1AntsDir -w $wmhFile -t $t1File -f $t1Filled -m $wmMask -a $asegFile"
     echo  Universe = vanilla > $condorJobFile
     echo  Executable = $jobScript >> $condorJobFile
     echo  Arguments ="$jobArgs" >> $condorJobFile
